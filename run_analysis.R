940235ce15d046c9c9bac7606519d0b53c0b43ca
Mahmoud Badreddine <runnerbrain@gmail.com>
2015-10-25 13:11:14 -0400

    Second submission of the run_analysis.R script with Comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,12 @@
+##################################################################
+## Purpose : to extract and tidy data from a dataset
+##           and give a consise summary of partial data elements
+##
+## Author : Mahmoud Badreddine
+## Date:  2015/10/25
+##
+##################################################################
+
     library(dplyr)
     library(data.table)
     
@@ -5,6 +14,8 @@
     # and x_test files.
     features_list <- read.table("./features.txt")
     activity_list <- read.table("./activity_labels.txt")
+    
+    #Assign table headers to the first two colums of the activity table
     names(activity_list) <- c("activity_id","activity_label")
     
     
@@ -13,15 +24,20 @@
     #Read in all the id's of the activity file.
     tr_activity_id <- read.table("./train/y_train.txt")
     
-    #column bind the subject id's and the activity id's in a table which will when merged shows a cerain subject doing
-    # a certain activity
+    #column bind the subject id's and the activity id's in a table which when merged associates a subject to an activity
+    # and store in data frame called train_ids
     train_ids <- cbind(tr_subject_id,tr_activity_id)
+    
+    #Create a new column which will hold descriptive textual value of the activity performed.
     named_activity <- c("")
+    
+    #Concatenate the named activity column (which is empty at this point) to the first two columns of id's of subject and activity respectively.
     train_ids <- cbind(train_ids,named_activity)
     
-    
-    #Give names to the id's
+    #Assign table headers to the first three columns 
     names(train_ids) <- c("subject_id","activity_id","activity_label")
+    
+    #Populate the activity_label column with values based on the activity list. 1 => Walking, 2 => Walking upstairs....etc.
     train_ids <- mutate(train_ids, activity_label = ifelse(activity_id == 1, "WALKING",
                                                            ifelse(activity_id == 2, "WALKING_UPSTAIRS",
                                                                   ifelse(activity_id == 3, "WALKING_DOWNSTAIRS",
@@ -32,15 +48,17 @@
     #read in the x_train.txt file into the x_train data frame
     x_train <- read.table("./train/x_train.txt")
     
-    #set the names of the x_train data frame created above to the rows of the features list data frame created in step ??? above
+    #Assign table headers of the x_train data frame using the rows of the second column of the features list created above.
     names(x_train) <- features_list[,2]
     
-    #merge using column bind the id's of the subject and the correpsonding id's with the features data frame.
+    #merge using column bind, all the columns of the train_ids data frame created above with the features data frame x_train created above.
     train_dataset <- cbind(train_ids,x_train)
+    
     #View(train_dataset)
     
     
-    #do the same thing for the test files.
+    #do the same thing for the files under the test directory and prefix files with tst to distinguish them from the files
+    #in the train directory
     tst_subject_id <- read.table("./test/subject_test.txt")
     tst_activity_id <- read.table("./test/y_test.txt")
     tst_ids <- cbind(tst_subject_id,tst_activity_id)
@@ -57,26 +75,46 @@
     names(x_test) <- features_list[,2]
     test_dataset <- cbind(tst_ids,x_test)
     
-    #row bind both train data frame and test data frames.
+    
+    #
+    #Merge both train and test data using row bind to produce one dataset and store it in a data frame called merged_dataset
     merged_dataset <- rbind(train_dataset,test_dataset)
     
-    #extract the headers with the word std and mean from the features list and store them into a vector
+    #Convert the features list to a datatable to be ablve to use certain queries on it.
     features_dt <- data.table(features_list)
     
+    
+    #extract the headers with the word std and mean from the features list and store them in a data frames std_headers and mean_headers respectively.
     mean_headers <- features_dt[V2 %like% "mean"]
     std_headers <- features_dt[V2 %like% "std"]
     
+    #Combine both std and mean headers in a single dataframe called headers
     headers <- rbind(mean_headers,std_headers)
+    
+    #create a new headers list which will make up only the numerical value of the final data frame which excludes all the ID's values 
+    # and store it in a right_headers_list.
     right_headers_list <- as.vector(headers[,V2])
+    
+    #creat a headers list made up of only the id columns information called left headers list.
     left_headers_list <- c("subject_id","activity_id","activity_label")
+    
+    #combine both as character vector called all_headers
     all_headers <- c(left_headers_list,right_headers_list)
+    
+    #extract part of the dataset based on the headers created in the previous step.
     partial_data <- merged_dataset[,all_headers]
-    partial_data_r <- merged_dataset[,right_headers_list]
+    
+    #Using dplyr tools apply group_by on the partial_data data frame to group by both subject_id 
+    #and activity_id then using summarize_each apply the function mean to produce the average fore each column based
+    #on that grouping.
     summary_by_subject_activity <- partial_data %>%
       group_by(subject_id,activity_id,activity_label) %>%
       summarize_each(funs(mean))
     
+    #View the summary
     View(summary_by_subject_activity)
+    
+    #write it as a text file which will be uploaded and submitted for gradin as per the requirements of the project.
     summary_by_sbject_activity.txt <- write.table(summary_by_subject_activity,file="./summary_by_subject_activity.txt",row.name = FALSE)  
     
     
